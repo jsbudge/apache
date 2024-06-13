Redid the change in directory structure. Fixed some of the scaling stuff for target encoded data. Got the target encoder up and running.

dataloaders.py
@@ -74,6 +74,30 @@ class RCSDataset(Dataset):
 
     def __len__(self):
         return self.optical_data.shape[0]
+    
+    
+class TargetDataset(Dataset):
+    def __init__(self, datapath: str = './data', split: float = 1., is_val: bool = False, seed: int = 7):
+        # Load in data
+        optical_data = np.fromfile(f'{datapath}/targetprofiles.dat',
+                                   dtype=np.float32).reshape((-1, 2, 256, 256))
+        self.data = torch.tensor(optical_data)
+
+        if split < 1:
+            Xs, Xt, _, _ = train_test_split(np.arange(optical_data.shape[0]),
+                                            np.arange(optical_data.shape[0]),
+                                            test_size=split, random_state=seed)
+        else:
+            Xt = np.arange(optical_data.shape[0])
+            Xs = np.arange(optical_data.shape[0])
+        self.data = torch.tensor(optical_data[Xs], dtype=torch.float32) if is_val else (
+            torch.tensor(optical_data[Xt], dtype=torch.float32))
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.data[idx]
+
+    def __len__(self):
+        return self.data.shape[0]
 
 
 class WaveDataset(ConcatDataset):
@@ -181,6 +205,7 @@ class BaseModule(LightningDataModule):
             pin_memory: bool = False,
             single_example: bool = False,
             device: str = 'cpu',
+            collate: bool = False,
             **kwargs,
     ):
         super().__init__()
@@ -193,29 +218,48 @@ class BaseModule(LightningDataModule):
         self.pin_memory = pin_memory
         self.single_example = single_example
         self.device = device
+        self.collate = collate
 
     def setup(self, stage: Optional[str] = None) -> None:
         pass
 
     def train_dataloader(self) -> DataLoader:
-        return DataLoader(
-            self.train_dataset,
-            batch_size=self.train_batch_size,
-            num_workers=self.num_workers,
-            shuffle=True,
-            pin_memory=self.pin_memory,
-            collate_fn=collate_fun,
-        )
+        if self.collate:
+            return DataLoader(
+                self.train_dataset,
+                batch_size=self.train_batch_size,
+                num_workers=self.num_workers,
+                shuffle=True,
+                pin_memory=self.pin_memory,
+                collate_fn=collate_fun,
+            )
+        else:
+            return DataLoader(
+                self.train_dataset,
+                batch_size=self.train_batch_size,
+                num_workers=self.num_workers,
+                shuffle=True,
+                pin_memory=self.pin_memory,
+            )
 
     def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
-        return DataLoader(
-            self.val_dataset,
-            batch_size=self.val_batch_size,
-            num_workers=self.num_workers,
-            shuffle=False,
-            pin_memory=self.pin_memory,
-            collate_fn=collate_fun,
-        )
+        if self.collate:
+            return DataLoader(
+                self.val_dataset,
+                batch_size=self.val_batch_size,
+                num_workers=self.num_workers,
+                shuffle=False,
+                pin_memory=self.pin_memory,
+                collate_fn=collate_fun,
+            )
+        else:
+            return DataLoader(
+                self.val_dataset,
+                batch_size=self.val_batch_size,
+                num_workers=self.num_workers,
+                shuffle=False,
+                pin_memory=self.pin_memory,
+            )
 
     def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
         return DataLoader(
@@ -313,6 +357,31 @@ class EncoderModule(BaseModule):
         self.train_dataset = PulseDataset(self.data_path, self.fft_len, self.split, self.single_example)
         self.val_dataset = PulseDataset(self.data_path, self.fft_len, split=1 - self.split if self.split < 1 else 1.,
                                         single_example=self.single_example, is_val=True)
+        
+        
+class TargetEncoderModule(BaseModule):
+    def __init__(
+            self,
+            data_path,
+            split: float = 1.,
+            dataset_size: int = 256,
+            train_batch_size: int = 8,
+            val_batch_size: int = 8,
+            pin_memory: bool = False,
+            single_example: bool = False,
+            device: str = 'cpu',
+            **kwargs,
+    ):
+        super().__init__(train_batch_size, val_batch_size, pin_memory, single_example, device)
+
+        self.dataset_size = dataset_size
+        self.data_path = data_path
+        self.split = split
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        self.train_dataset = TargetDataset(self.data_path, self.split, self.single_example)
+        self.val_dataset = TargetDataset(self.data_path, split=1 - self.split if self.split < 1 else 1.,
+                                         is_val=True)
 
 
 if __name__ == '__main__':

models.py
@@ -895,26 +895,26 @@ class TargetEncoder(FlatModule):
     def __init__(self,
                  in_channels: int,
                  latent_dim: int,
-                 fft_len: int,
                  params: dict,
                  channel_sz: int = 32,
                  **kwargs) -> None:
         super(TargetEncoder, self).__init__()
 
         self.latent_dim = latent_dim
-        self.fft_len = fft_len
         self.params = params
         self.channel_sz = channel_sz
         self.in_channels = in_channels
         self.automatic_optimization = False
-        levels = 4
+        levels = 3
+        out_sz = 256 // (2**levels)
 
         # Encoder
         self.encoder_inflate = nn.Conv2d(in_channels, channel_sz, 3, 1, 1)
         self.encoder_reduce = nn.ModuleList()
         self.encoder_conv = nn.ModuleList()
         self.encoder_attention = nn.ModuleList()
-        for _ in range(levels):
+        for l in range(levels):
+            layer_sz = [256 // (2**(l + 1)), 256 // (2**(l + 1))]
             self.encoder_reduce.append(nn.Sequential(
                 nn.Conv2d(channel_sz, channel_sz, 4, 2, 1),
                 nn.GELU(),
@@ -922,18 +922,34 @@ class TargetEncoder(FlatModule):
             self.encoder_conv.append(nn.Sequential(
                 nn.Conv2d(channel_sz, channel_sz, 3, 1, 1),
                 nn.GELU(),
+                nn.Conv2d(channel_sz, channel_sz, 3, 1, 1),
+                nn.GELU(),
+                nn.Conv2d(channel_sz, channel_sz, 3, 1, 1),
+                nn.GELU(),
+                nn.Conv2d(channel_sz, channel_sz, 3, 1, 1),
+                nn.GELU(),
+                nn.LayerNorm(layer_sz),
             ))
-        self.fc_z = nn.Sequential(
+        self.encoder_flatten = nn.Sequential(
             nn.Conv2d(channel_sz, 1, 1, 1, 0),
+            nn.GELU(),
+        )
+        self.fc_z = nn.Sequential(
+            nn.Linear(out_sz**2, latent_dim),
             nn.Tanh(),
         )
 
         # Decoder
-        self.z_fc = nn.Conv2d(1, channel_sz, 1, 1, 0)
+        self.z_fc = nn.Linear(latent_dim, out_sz**2)
+        self.decoder_flatten = nn.Sequential(
+            nn.Conv2d(1, channel_sz, 1, 1, 0),
+            nn.GELU(),
+        )
         self.decoder_reduce = nn.ModuleList()
         self.decoder_conv = nn.ModuleList()
         self.decoder_attention = nn.ModuleList()
-        for _ in range(levels):
+        for l in range(levels):
+            layer_sz = [out_sz * (2 ** l), out_sz * (2 ** l)]
             self.decoder_reduce.append(nn.Sequential(
                 nn.ConvTranspose2d(channel_sz, channel_sz, 4, 2, 1),
                 nn.GELU(),
@@ -941,12 +957,21 @@ class TargetEncoder(FlatModule):
             self.decoder_conv.append(nn.Sequential(
                 nn.ConvTranspose2d(channel_sz, channel_sz, 3, 1, 1),
                 nn.GELU(),
+                nn.ConvTranspose2d(channel_sz, channel_sz, 3, 1, 1),
+                nn.GELU(),
+                nn.ConvTranspose2d(channel_sz, channel_sz, 3, 1, 1),
+                nn.GELU(),
+                nn.ConvTranspose2d(channel_sz, channel_sz, 3, 1, 1),
+                nn.GELU(),
+                nn.LayerNorm(layer_sz),
             ))
         self.decoder_output = nn.Sequential(
             nn.ConvTranspose2d(channel_sz, in_channels, 1, 1, 0),
         )
 
-        self.example_input_array = torch.randn((1, 2, self.fft_len))
+        self.latent_dim = latent_dim
+        self.out_sz = out_sz
+        self.example_input_array = torch.randn((2, 256, 256))
 
     def encode(self, inp: Tensor) -> Tensor:
         """
@@ -958,10 +983,12 @@ class TargetEncoder(FlatModule):
         inp = self.encoder_inflate(inp)
         for conv, red in zip(self.encoder_conv, self.encoder_reduce):
             inp = conv(red(inp))
+        inp = self.encoder_flatten(inp).view(-1, self.out_sz**2)
         return self.fc_z(inp)
 
     def decode(self, z: Tensor) -> Tensor:
-        result = self.z_fc(z)
+        result = self.z_fc(z).view(-1, 1, self.out_sz, self.out_sz)
+        result = self.decoder_flatten(result)
         for conv, red in zip(self.decoder_conv, self.decoder_reduce):
             result = red(conv(result))
         return self.decoder_output(result)
@@ -971,7 +998,7 @@ class TargetEncoder(FlatModule):
         return self.decode(z)
 
     def loss_function(self, y, y_pred):
-        return tf.mse_loss(y, y_pred)
+        return tf.cross_entropy(y, y_pred)
 
     def on_fit_start(self) -> None:
         if self.trainer.is_global_zero and self.logger:
@@ -998,24 +1025,7 @@ class TargetEncoder(FlatModule):
             print('Model saved to disk.')
 
             if self.current_epoch % 5 == 0:
-                # Log an image to get an idea of progress
-                img, _ = next(iter(self.trainer.val_dataloaders))
-                rec = self.forward(img.to(self.device))
-                rec = rec.to('cpu').data.numpy()
-                fig = plt.figure()
-                plt.subplot(2, 2, 1)
-                plt.title('Real Original')
-                plt.plot(img[0, 0, :])
-                plt.subplot(2, 2, 2)
-                plt.title('Imag Original')
-                plt.plot(img[0, 1, :])
-                plt.subplot(2, 2, 3)
-                plt.title('Real Reconstructed')
-                plt.plot(rec[0, 0, :])
-                plt.subplot(2, 2, 4)
-                plt.title('Imag Reconstructed')
-                plt.plot(rec[0, 1, :])
-                self.logger.experiment.add_figure('Reconstruction', fig, self.current_epoch)
+                pass
 
     def on_train_epoch_end(self) -> None:
         if self.trainer.is_global_zero and not self.params['is_tuning'] and self.params['loss_landscape']:

target_train.py
@@ -3,6 +3,7 @@ from clearml import Task
 import torch
 from pytorch_lightning import Trainer, loggers, seed_everything
 from pytorch_lightning.callbacks import EarlyStopping, StochasticWeightAveraging
+from simulib.simulation_functions import db
 import yaml
 from dataloaders import TargetEncoderModule
 from models import init_weights, TargetEncoder
@@ -18,14 +19,14 @@ seed_everything(43, workers=True)
 with open('./vae_config.yaml') as y:
     param_dict = yaml.safe_load(y.read())
 
-exp_params = param_dict['exp_params']
+exp_params = param_dict['target_exp_params']
 # hparams = make_dataclass('hparams', param_dict.items())(**param_dict)
 
 data = TargetEncoderModule(**exp_params["dataset_params"])
 data.setup()
 
 # Get the model, experiment, logger set up
-model = TargetEncoder(**param_dict['model_params'], fft_len=param_dict['settings']['fft_len'], params=exp_params)
+model = TargetEncoder(**exp_params['model_params'], params=exp_params)
 print('Setting up model...')
 tag_warm = 'new_model'
 if exp_params['warm_start']:
@@ -39,16 +40,15 @@ if exp_params['warm_start']:
 else:
     print('Initializing new model...')
     model.apply(init_weights)
-
-task = Task.init(project_name='TargetEncoder', task_name=param_dict['exp_params']['exp_name'])
+if exp_params['init_task']:
+    task = Task.init(project_name='TargetEncoder', task_name=exp_params['exp_name'])
 
 logger = loggers.TensorBoardLogger(param_dict['train_params']['log_dir'], name="TargetEncoder")
 expected_lr = max((exp_params['LR'] *
                    exp_params['scheduler_gamma'] ** (exp_params['max_epochs'] *
                                                      exp_params['swa_start'])), 1e-9)
 trainer = Trainer(logger=logger, max_epochs=exp_params['max_epochs'],
-                  log_every_n_steps=exp_params['log_epoch'],
-                  strategy='ddp', devices=1, callbacks=
+                  log_every_n_steps=exp_params['log_epoch'], devices=1, callbacks=
                   [EarlyStopping(monitor='val_loss', patience=exp_params['patience'],
                                  check_finite=True),
                    StochasticWeightAveraging(swa_lrs=expected_lr, swa_epoch_start=exp_params['swa_start'])])
@@ -78,18 +78,12 @@ if trainer.is_global_zero:
             print(f'Model not saved: {e}')
     print('Plotting outputs...')
     plt.figure()
-    plt.subplot(2, 2, 1)
-    plt.title('Sample Real')
-    plt.plot(sample[0, ...])
-    plt.subplot(2, 2, 2)
-    plt.title('Sample Imag')
-    plt.plot(sample[1, ...])
-    plt.subplot(2, 2, 3)
-    plt.title('Recon Real')
-    plt.plot(recon[0, ...])
-    plt.subplot(2, 2, 4)
-    plt.title('Recon Imag')
-    plt.plot(recon[1, ...])
+    plt.subplot(2, 1, 1)
+    plt.title('Sample')
+    plt.imshow(db(sample[0] + 1j * sample[1]))
+    plt.subplot(2, 1, 2)
+    plt.title('Recon')
+    plt.imshow(db(recon[0] + 1j * recon[1]))
     plt.show()
 
     if exp_params['transform_data']:
@@ -119,4 +113,5 @@ if trainer.is_global_zero:
             for td in np.arange(0, target_data.shape[0], batch_sz):
                 out_data = model.encode(torch.tensor(target_data[td:td + batch_sz, ...])).data.numpy()
                 out_data.tofile(writer)
-task.close()
+if exp_params['init_task']:
+    task.close()