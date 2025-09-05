Modernized lots of scripts. Also fixed some decimation stuff and got gradient flow working properly.

gui/simulator_window.py
@@ -556,9 +556,8 @@ class SimulationThread(QThread):
                 os.mkdir(tensor_path)
             mf_chirp = np.fft.fft(wavedata, self.fft_len)
             mf_chirp = mf_chirp * mf_chirp.conj()
-            streams = [cuda.stream() for _ in range(1)]
             self.signal_update_progress.emit('Loading mesh parameters...')
-            gen_iter = iter(genProfileFromMesh(self.mesh, self.iterations, [mf_chirp], 2**16, streams,
+            gen_iter = iter(genProfileFromMesh(self.mesh, self.iterations, [mf_chirp], 2**16, 0,
                        self.ranges, self.fft_len, self.radar_coeff, nsam, fs, fc, num_bounces=1,
                        a_naz=self.n_az_samples, a_nel=self.n_el_samples))
             self.signal_update_progress.emit('Running simulation...')

target_data_generator.py
@@ -3,10 +3,10 @@ import os
 import numpy as np
 from pathlib import Path
 from numba import cuda
-from simulib.mesh_objects import Scene, Mesh
 from simulib.simulation_functions import azelToVec, genChirp
-from simulib.mesh_functions import readCombineMeshFile, getRangeProfileFromScene, _float
+from simulib.mesh_functions import getRangeProfileFromScene
 from simulib.platform_helper import SDRPlatform
+from simulib.utils import _float, _complex_float
 from scipy.signal.windows import taylor, tukey
 import plotly.io as pio
 from tqdm import tqdm
@@ -16,13 +16,11 @@ import torch
 import matplotlib as mplib
 import pickle
 from config import load_yaml_config
-
 mplib.use('TkAgg')
 from sklearn.decomposition import TruncatedSVD
 import matplotlib.pyplot as plt
 from simulib.simulation_functions import db
 import pandas as pd
-
 from utils import scale_normalize, get_radar_coeff, normalize
 
 pio.renderers.default = 'browser'
@@ -41,7 +39,7 @@ MAX_EL_BEAMWIDTH = 25 * DTR
 
 
 def formatTargetClutterData(data: np.ndarray, bin_bandwidth: int):
-    split = np.zeros((data.shape[0], 2, bin_bandwidth), dtype=np.float32)
+    split = np.zeros((data.shape[0], 2, bin_bandwidth), dtype=_float)
     split[:, 0, :bin_bandwidth // 2] = data[:, -bin_bandwidth // 2:].real
     split[:, 0, -bin_bandwidth // 2:] = data[:, :bin_bandwidth // 2].real
     split[:, 1, :bin_bandwidth // 2] = data[:, -bin_bandwidth // 2:].imag
@@ -70,7 +68,7 @@ def readVCS(filepath):
     return _scat_data, np.array(_angles) * DTR
 
 
-def genProfileFromMesh(a_scene, niters, a_mf_chirp, a_points_to_sample, a_streams,
+def genProfileFromMesh(a_scene, niters, a_mf_chirp, a_points_to_sample, a_supersamples,
                        a_standoff, a_fft_len, a_radarc, a_nsam, a_fs, a_fc, num_bounces=1,
                        a_naz=8, a_nel=8):
 
@@ -82,9 +80,9 @@ def genProfileFromMesh(a_scene, niters, a_mf_chirp, a_points_to_sample, a_stream
         chirp_idx = np.random.randint(0, len(a_mf_chirp))
         az_bw = MAX_AZ_BEAMWIDTH * np.random.rand() + DTR
         el_bw = MAX_EL_BEAMWIDTH * np.random.rand() + DTR
-        block = np.zeros((len(a_standoff), poses.shape[0], a_fft_len), dtype=np.complex64)
+        block = np.zeros((len(a_standoff), poses.shape[0], a_fft_len), dtype=_complex_float)
         for k, s in enumerate(a_standoff):
-            msrp, block[k] = getTargetProfile(a_scene, m_sample_points, s, a_streams, num_bounces,
+            msrp, block[k] = getTargetProfile(a_scene, m_sample_points, s, a_supersamples, num_bounces,
                                               a_mf_chirp[chirp_idx], a_naz, a_nel, a_radarc, a_nsam, a_fs, a_fc,
                                               a_fft_len, az_bw, el_bw)
         yield msrp, block, m_i
@@ -97,7 +95,7 @@ def genProfileFromVCS(a_obj_path, a_standoff, a_niters, a_mf_chirp):
     # Generate target profile on CPU
     for m_i in range(a_niters):
         chirp_idx = np.random.randint(0, len(a_mf_chirp))
-        block = np.zeros((len(a_standoff), n_az_samples * n_el_samples, fft_len), dtype=np.complex64)
+        block = np.zeros((len(a_standoff), n_az_samples * n_el_samples, fft_len), dtype=_complex_float)
         for k, s in enumerate(a_standoff):
             m_near_range_s, poses, bore_az, bore_el = calcPosBoresight(s)
             m_bore_ang = np.array([bore_az, bore_el]).T
@@ -153,24 +151,23 @@ def loadClutterFiles():
     return m_clutter_files
 
 
-def loadClutterTargetSpectrum(a_clut, a_radarc, scene, iterations, seq_min, seq_max, a_points_to_sample, fc, num_bounces, fft_len=8192, a_streams=None):
-    sdr_ch = load(a_clut, import_pickle=False, use_jump_correction=False)
+def loadClutterTargetSpectrum(a_clut, a_radarc, scene, iterations, seq_min, seq_max, a_points_to_sample, fc,
+                              num_bounces, fft_len=8192):
+    sdr_ch = load(a_clut, import_pickle=True, use_jump_correction=False)
     rp = SDRPlatform(sdr_ch)
     m_nsam, nr, ranges, ranges_sampled, near_range_s, granges, full_fft_len, up_fft_len = (
-        rp.getRadarParams(0., .5, 1))
+        rp.getRadarParams(0., .21, 1))
+    scene.shift(np.array([0., 0., 0.]), relative=False)
 
     downsample_rate = full_fft_len // fft_len
 
     flight_path = rp.rxpos(sdr_ch[0].pulse_time)
-    # Locate the extrema to speed up the optimization
 
-    nstreams = len(a_streams)
     sample_points = scene.sample(a_points_to_sample, a_obs_pts=flight_path[::10])
-    sample_center = scene.center + 0.
 
     # Get pulse data and modify accordingly
-    pulse = np.fft.fft(sdr_ch[0].cal_chirp, fft_len)
-    mfilt = sdr_ch.genMatchedFilter(0, fft_len=fft_len)
+    pulse = np.fft.fft(sdr_ch[0].cal_chirp, full_fft_len)
+    mfilt = sdr_ch.genMatchedFilter(0, fft_len=full_fft_len)
     for _ in range(iterations):
         pt_pt = np.random.choice(sdr_ch[0].nframes - seq_max)
         frame_idxes = np.arange(pt_pt, pt_pt + np.random.choice(np.arange(seq_min, seq_max)))
@@ -184,30 +181,52 @@ def loadClutterTargetSpectrum(a_clut, a_radarc, scene, iterations, seq_min, seq_
 
         mpos = floc + r_d * proj_rng
 
-        txposes = [rp.txpos(pulse_times).astype(_float) for _ in range(nstreams)]
-        rxposes = [rp.rxpos(pulse_times).astype(_float) for _ in range(nstreams)]
-        pans = [rp.pan(pulse_times).astype(_float) for _ in range(nstreams)]
-        tilts = [rp.tilt(pulse_times).astype(_float) for _ in range(nstreams)]
-        sdr_data = [sdr_ch.getPulses(sdr_ch[0].frame_num[frame_idxes], 0)[1] for _ in range(nstreams)]
+        txposes = [rp.txpos(pulse_times).astype(_float)]
+        rxposes = [rp.rxpos(pulse_times).astype(_float)]
+        pans = [rp.pan(pulse_times).astype(_float)]
+        tilts = [rp.tilt(pulse_times).astype(_float)]
+        sdr_data = [sdr_ch.getPulses(sdr_ch[0].frame_num[frame_idxes], 0)[1]]
 
         scene.shift(mpos, relative=False)
-        sample_points = sample_points - sample_center + scene.center
-        sample_center = scene.center + 0.
+        shifted_points = [sample_points + scene.center]
 
-        single_rp = getRangeProfileFromScene(scene, sample_points, txposes, rxposes, pans, tilts,
+        single_rp = getRangeProfileFromScene(scene, shifted_points, txposes, rxposes, pans, tilts,
                                              a_radarc, rp.az_half_bw, rp.el_half_bw, m_nsam,
                                              fc, near_range_s, fs=fs,
-                                             num_bounces=num_bounces, streams=a_streams)
-        tpsds = [np.fft.fft(srp[:, ::downsample_rate], fft_len, axis=1) * mfilt * pulse for srp in single_rp]
+                                             num_bounces=num_bounces, supersamples=0)
+        tpsds = [[np.fft.fft(s[:, ::downsample_rate], fft_len, axis=1) * mfilt[::downsample_rate] * pulse[::downsample_rate] for s in srp] for srp in single_rp][0]
         for sdrd, tpsd in zip(sdr_data, tpsds):
             if len(tpsd[tpsd != 0]) == 0:
                 continue
-            m_sdata = np.fft.fft(sdrd, fft_len, axis=0).T * mfilt
+            m_sdata = np.fft.fft(sdrd, fft_len, axis=0).T * mfilt[::downsample_rate]
             if tpsd[tpsd != 0].std() == 0 or m_sdata[m_sdata != 0].std() == 0:
                 continue
-            # Unit energy and scale to have std of one
-            m_ntpsd = normalize(tpsd) * 1e2
-            m_sdata = normalize(m_sdata) * 1e2
+
+            sdtime = np.fft.ifft(m_sdata, axis=1)
+            tptime = np.fft.ifft(tpsd, axis=1)
+            # dbtime = db(np.fft.ifft(tpsd, axis=1))
+            up_fac = (abs(sdtime).mean(axis=1) + abs(sdtime).std(axis=1) * 6) / np.max(abs(tptime), axis=1)
+            comb = sdtime + tptime * up_fac[:, None]
+
+            comb = np.fft.fft(comb, axis=1)
+
+            '''plt.figure()
+            plt.subplot(3, 1, 1)
+            plt.title('sd')
+            plt.imshow(db(sdtime))
+            plt.axis('tight')
+            plt.subplot(3, 1, 2)
+            plt.title('sd')
+            plt.imshow(db(tptime))
+            plt.axis('tight')
+            plt.subplot(3, 1, 3)
+            plt.title('sd')
+            plt.imshow(db(comb))
+            plt.axis('tight')'''
+            # Unit energy and scale to have std of one based on the original
+            norm_factor = np.expand_dims(np.sqrt(np.sum(m_sdata * m_sdata.conj(), axis=-1).real), axis=len(m_sdata.shape) - 1)
+            m_ntpsd = comb / norm_factor
+            m_sdata = m_sdata / norm_factor
             # Shift the data so it's centered around zero (for the autoencoder)
             if sdr_ch[0].baseband_fc != 0.:
                 shift_bin = int(sdr_ch[0].baseband_fc / sdr_ch[0].fs * fft_len)
@@ -218,17 +237,16 @@ def loadClutterTargetSpectrum(a_clut, a_radarc, scene, iterations, seq_min, seq_
             yield m_ntpsd, m_sdata
 
 
-def getTargetProfile(a_scene, a_sample_points, a_standoff, a_streams, a_num_bounces, a_mf_chirp, a_naz, a_nel, a_radarc,
+def getTargetProfile(a_scene, a_sample_points, a_standoff, a_supersamples, a_num_bounces, a_mf_chirp, a_naz, a_nel, a_radarc,
                      a_nsam, a_fs, a_fc, a_fft_len, a_azbw, a_elbw):
     m_near_range_s, poses, m_pan, m_tilt = calcPosBoresight(a_standoff, a_naz=a_naz, a_nel=a_nel)
     m_rxposes = poses + 0.0
-    m_single_rp, ray_origins, ray_directions, ray_powers = getRangeProfileFromScene(a_scene, a_sample_points, [poses.astype(_float)],
+    m_single_rp, ray_origins, ray_directions, ray_powers = getRangeProfileFromScene(a_scene, [a_sample_points], [poses.astype(_float)],
                                            [m_rxposes.astype(_float)], [m_pan.astype(_float)],
                                            [m_tilt.astype(_float)], a_radarc, a_azbw, a_elbw, a_nsam,
                                            a_fc, m_near_range_s, fs=a_fs, num_bounces=a_num_bounces,
-                                           streams=a_streams, debug=True)
-    msrp = m_single_rp[
-        0]  # + (np.random.randn(*m_single_rp[0].shape) + 1j * np.random.randn(*m_single_rp[0].shape)) * abs(m_single_rp[0][m_single_rp[0] > 0.]).mean() / 10
+                                           supersamples=a_supersamples, debug=True)
+    msrp = m_single_rp[0][0]
     return msrp, np.fft.fft(msrp, a_fft_len, axis=1) * a_mf_chirp
 
 
@@ -266,8 +284,6 @@ if __name__ == '__main__':
     # target_info = pd.read_csv('./data/target_info.csv')
 
     tsvd = TruncatedSVD(n_components=cfig_generate.n_el_samples * cfig_generate.n_az_samples)
-
-    streams = [cuda.stream() for _ in range(cfig_generate.nstreams)]
 
     # Standardize the FFT length for training purposes (this may cause data loss)
     fft_len = cfig_settings.fft_len
@@ -322,7 +338,7 @@ if __name__ == '__main__':
             # bandwidths and pulse lengths.
             if True:
                 gen_iter = iter(genProfileFromMesh(model, iterations, mf_chirp,
-                                                   2**cfig_generate.num_sample_points_power, streams, standoff, fft_len,
+                                                   2**cfig_generate.num_sample_points_power, cfig_generate.supersamples, standoff, fft_len,
                                                    radar_coeff, cfig_generate.nsam, fs, cfig_generate.fc,
                                                    cfig_generate.num_bounces, a_naz=n_az_samples, a_nel=n_el_samples))
             else:
@@ -350,14 +366,18 @@ if __name__ == '__main__':
             tensor_clutter_path = f"{config.target_exp_params.dataset_params.data_path}/clutter_tensors"
             if not Path(tensor_clutter_path).exists():
                 os.mkdir(tensor_clutter_path)
-            print(f'Saving clutter spec for target {tname}')
+            print(f'Clutter spec for target {tname}')
             for clut in clutter_files:
                 # Load the sar file that the clutter came from
+                print(f'{clut}')
                 for ntpsd, sdata in loadClutterTargetSpectrum(clut, radar_coeff, model, cfig_generate.clutter_iterations,
                                                                 cfig_generate.seq_min_length, cfig_generate.seq_max_length,
-                                                              2**cfig_generate.num_sample_points_power, cfig_generate.fc, cfig_generate.num_bounces,
-                                                              a_streams=streams):
+                                                              2**cfig_generate.num_sample_points_power, cfig_generate.fc, cfig_generate.num_bounces):
+                    '''plt.figure(f'{clut}')
+                    plt.plot(db(ntpsd[0, 0]))
+                    plt.plot(db(sdata[0, 0]))'''
                     if cfig_generate.save_files and (not np.any(np.isnan(ntpsd)) and not np.any(np.isnan(sdata))):
+                        # Generate a list with sdata (sdr pulses), ntpsd (target profile added to sdr pulse) and tidx (target index)
                         torch.save([torch.tensor(sdata, dtype=torch.float32),
                                     torch.tensor(ntpsd, dtype=torch.float32), tidx],
                                    f"{tensor_clutter_path}/tc_{abs_clutter_idx}.pt")

transform_data.py
@@ -1,68 +1,65 @@
-from target_train import loadModel
-from glob import glob
-import os
+from config import get_config
+from utils import upsample, normalize, fs, narrow_band
 import numpy as np
-from pathlib import Path
-from simulib.simulation_functions import db, azelToVec, genChirp, getElevation
-from simulib.cuda_mesh_kernels import readCombineMeshFile, getBoxesSamplesFromMesh, getRangeProfileFromMesh
-from simulib.platform_helper import SDRPlatform
-from simulib.cuda_kernels import cpudiff, getMaxThreads
-from generate_trainingdata import formatTargetClutterData
-from scipy.signal.windows import taylor
+from simulib.simulation_functions import genPulse, db, findPowerOf2
 import matplotlib.pyplot as plt
-import plotly.io as pio
-from tqdm import tqdm
-import yaml
-from data_converter.SDRParsing import load
+from scipy.signal import stft
 import torch
-from pytorch_lightning import seed_everything
-import cupy
+from pytorch_lightning import Trainer, loggers, seed_everything
+from pytorch_lightning.callbacks import EarlyStopping, StochasticWeightAveraging, ModelCheckpoint
+from dataloaders import WaveDataModule
+from waveform_model import GeneratorModel
+
+
+def force_cudnn_initialization():
+    s = 32
+    dev = torch.device('cuda')
+    torch.nn.functional.conv2d(torch.zeros(s, s, s, s, device=dev), torch.zeros(s, s, s, s, device=dev))
 
 if __name__ == '__main__':
     torch.set_float32_matmul_precision('medium')
-    gpu_num = 1
-    device = f'cuda:{gpu_num}' if torch.cuda.is_available() else 'cpu'
-    seed_everything(np.random.randint(1, 2048), workers=True)
-    # seed_everything(43, workers=True)
+    torch.autograd.set_detect_anomaly(True)
+    force_cudnn_initialization()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # torch.cuda.empty_cache()
 
-    with open('./vae_config.yaml') as y:
-        param_dict = yaml.safe_load(y.read())
+    # seed_everything(np.random.randint(1, 2048), workers=True)
+    seed_everything(107, workers=True)
 
-    # First, get a representation of the targets from the embedding model
-    exp_params = param_dict['target_exp_params']
+    config = get_config('wave_exp', './vae_config.yaml')
+    config.gradient_flow = True
 
-    trainer, model, data, task = loadModel(exp_params, gpu_num, param_dict['settings']['fft_len'], True, 'target', param_dict['train_params']['log_dir'])
-    model.to(device)
-    model.eval()
+    fft_len = config.fft_len
+    nr = 5000  # int((config['perf_params']['vehicle_slant_range_min'] * 2 / c0 - 1 / TAC) * fs)
+    # Since these are dependent on apache params, we set them up here instead of in the yaml file
+    print('Setting up data generator...')
+    config.dataset_params['max_pulse_length'] = nr
+    config.dataset_params['min_pulse_length'] = 1000
 
-    ntargets = exp_params['dataset_params']['train_batch_size'] // 2
+    data = WaveDataModule(device=device, **config.dataset_params)
+    data.setup()
 
-    embeddings = np.zeros((ntargets, exp_params['model_params']['latent_dim']))
-    print('Iterating train dataloader...')
-    val_gen = iter(data.train_dataloader())
-    for i, sam in tqdm(enumerate(val_gen)):
-        single_emb = model(sam[0].to(device)).cpu().data.numpy()
-        embeddings += single_emb[:ntargets]
-        embeddings += single_emb[ntargets:]
+    print('Initializing wavemodel...')
+    wave_mdl = GeneratorModel(config=config)
 
-    print('Iterating val dataloader...')
-    val_gen = iter(data.val_dataloader())
-    nemb = i + 1
-    for i, sam in tqdm(enumerate(val_gen)):
-        single_emb = model(sam[0].to(device)).cpu().data.numpy()
-        embeddings += single_emb[:ntargets]
-        embeddings += single_emb[ntargets:]
-
-    embeddings /= (nemb + i + 1) * 2
-
-    model.to('cpu')
-
-    save_path = param_dict['generate_data_settings']['local_path'] if (
-        param_dict)['generate_data_settings']['use_local_storage'] else param_dict['dataset_params']['data_path']
-    torch.save([torch.tensor(embeddings, dtype=torch.float32)],
-               f"{save_path}/target_embedding_means.pt")
+    expected_lr = max((config.lr * config.scheduler_gamma ** (config.max_epochs * config.swa_start)), 1e-9)
+    trainer = Trainer(logger=None, num_sanity_val_steps=0, max_steps=1,
+                      default_root_dir=config.weights_path, check_val_every_n_epoch=1000,
+                      log_every_n_steps=config.log_epoch, devices=[1], callbacks=
+                      [EarlyStopping(monitor='target_loss', patience=config.patience, check_finite=True),
+                       StochasticWeightAveraging(swa_lrs=expected_lr, swa_epoch_start=config.swa_start),
+                       ModelCheckpoint(monitor='loss_epoch')])
+    print("======= Training =======")
+    try:
+        trainer.fit(wave_mdl, datamodule=data)
+    except KeyboardInterrupt:
+        if trainer.is_global_zero:
+            print('Training interrupted.')
+        else:
+            print('adios!')
+            exit(0)
 
 
-    # Now, load the clutter and target spec files for embedding
-    glob(f'{save_path}/clutter_*.spec')
+
+
 

utils.py
@@ -1,5 +1,6 @@
 import math
-
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 import numpy as np
 import torch
 from torch import nn
@@ -135,3 +136,43 @@ class ELiSH(nn.Module):
 
 nonlinearities = {'silu': nn.SiLU(), 'gelu': nn.GELU(), 'selu': nn.SELU(), 'leaky': nn.LeakyReLU(),
                   'grow': GrowingCosine(), 'elish': ELiSH()}
+
+
+def plot_grad_flow(named_parameters):
+    '''Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+
+    Usage: Plug this function in Trainer class after loss.backwards() as 
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
+    ave_grads = {}
+    max_grads = {}
+    layers = {}
+    for n, p in named_parameters:
+        if p.requires_grad and ("bias" not in n):
+            module_name = n.split('.')[0]
+            if module_name in layers:
+                layers[module_name].append(n)
+                ave_grads[module_name].append(p.grad.abs().mean().cpu().numpy())
+                max_grads[module_name].append(p.grad.abs().max().cpu().numpy())
+            else:
+                layers[module_name] = [n]
+                ave_grads[module_name] = [p.grad.abs().mean().cpu().numpy()]
+                max_grads[module_name] = [p.grad.abs().max().cpu().numpy()]
+    grid_sz = int(np.sqrt(len(layers)) + 1)
+    for idx, mn in enumerate(layers.keys()):
+        plt.subplot(grid_sz, grid_sz, idx + 1)
+        plt.title(mn)
+        plt.bar(np.arange(len(max_grads[mn])), max_grads[mn], alpha=0.1, lw=1, color="c")
+        plt.bar(np.arange(len(max_grads[mn])), ave_grads[mn], alpha=0.1, lw=1, color="b")
+        '''plt.hlines(0, 0, len(ave_grads[mn]) + 1, lw=2, color="k")
+        plt.xticks(range(len(ave_grads[mn])), layers, rotation="vertical")
+        plt.xlim(left=0, right=len(ave_grads[mn]))
+        # plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
+        plt.xlabel("Layers")
+        plt.ylabel("average gradient")
+        plt.title("Gradient flow")
+        plt.grid(False)
+        plt.legend([Line2D([0], [0], color="c", lw=4),
+                    Line2D([0], [0], color="b", lw=4),
+                    Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])'''
+    plt.show()

wave_train.py
@@ -142,10 +142,10 @@ if __name__ == '__main__':
                     np.fft.fftshift(clutter[0]) * waves[0, 0] * waves[0, 0].conj() + np.fft.fftshift(clutter[0]) * waves[
                         0, 1] * waves[0, 1].conj())
                 target_corr = np.fft.ifft(
-                    np.fft.fftshift(targets[0] + clutter[0]) * waves[0, 0] * waves[0, 0].conj() + np.fft.fftshift(targets[0] + clutter[0]) * waves[
+                    np.fft.fftshift(targets[0]) * waves[0, 0] * waves[0, 0].conj() + np.fft.fftshift(targets[0]) * waves[
                         0, 1] * waves[0, 1].conj())
                 linear_corr = np.fft.ifft(
-                    np.fft.fftshift(targets[0] + clutter[0]) * linear * linear.conj())
+                    np.fft.fftshift(targets[0]) * linear * linear.conj())
             else:
                 clutter_corr = np.fft.ifft(
                     np.fft.fftshift(clutter[0]) * waves[0, 0] * waves[0, 0].conj())
@@ -160,10 +160,8 @@ if __name__ == '__main__':
             plt.ylabel('Power (dB)')
 
             plt.figure('Target-Clutter Correlations')
-            comb_corr = np.fft.ifft(
-                np.fft.fftshift(targets[0] + clutter[0]) * waves[0, 0] * waves[0, 0].conj())
-            truth_corr = np.fft.ifft(
-                np.fft.fftshift(targets[0] + clutter[0]))
+            comb_corr = np.fft.fftshift(targets[0] + clutter[0])
+            truth_corr = np.fft.fftshift(clutter[0])
             plt.plot(db(comb_corr))
             plt.plot(db(truth_corr))
             plt.legend(['Comb', 'Truth'])

waveform_model.py
@@ -13,7 +13,7 @@ from config import Config
 from layers import FourierFeature, PulseLength, LKA1d, LKATranspose1d, WindowGenerate, PositionalEncoding
 import numpy as np
 
-from utils import normalize, get_pslr, _xavier_init, nonlinearities
+from utils import normalize, get_pslr, _xavier_init, nonlinearities, plot_grad_flow
 
 EXP_1 = 0.36787944117144233
 EPS = 1e-12
@@ -90,14 +90,13 @@ class GeneratorModel(FlatModule):
         # self.predict_lstm = nn.LSTM(self.target_latent_size, self.target_latent_size, self.lstm_layers, batch_first=True)
         self.predict_decoder = nn.Transformer(self.target_latent_size, num_decoder_layers=4, num_encoder_layers=4, nhead=8,
                                               batch_first=True, activation='gelu')
-        self.pos_enc = PositionalEncoding(self.target_latent_size, .1, 32)
+        self.pos_enc = PositionalEncoding(self.target_latent_size, .1, 16)
         # self.predict_decoder.apply(init_weights)
 
         '''CLUTTER AND TARGET COMBINATION LAYERS'''
         self.clutter_encoder = nn.Sequential(
             nn.Linear(self.fft_len, self.target_latent_size),
             nlin,
-            nn.LayerNorm(self.target_latent_size),
             nn.Linear(self.target_latent_size, self.target_latent_size),
             nlin,
         )
@@ -117,26 +116,30 @@ class GeneratorModel(FlatModule):
         self.target_layers = nn.ModuleList()
         for _ in range(config.n_skip_layers):
             self.waveform_layers.append(nn.Sequential(
-            TFNO1d(n_modes_height=self.n_fourier_modes, in_channels=self.flowthrough_channels + 2,
-                   out_channels=self.waveform_channels, non_linearity=nlin,
-                   hidden_channels=self.waveform_channels),
-            LKA1d(self.waveform_channels, kernel_sizes=(129, 129), dilation=6, activation=self.nonlinearity),
-            LKA1d(self.waveform_channels, kernel_sizes=(129, 129), dilation=6, activation=self.nonlinearity),
-            TFNO1d(n_modes_height=self.n_fourier_modes, in_channels=self.waveform_channels,
-                   out_channels=self.flowthrough_channels, non_linearity=nlin, hidden_channels=self.waveform_channels),
+                TFNO1d(n_modes_height=self.n_fourier_modes, in_channels=self.flowthrough_channels + 2,
+                       out_channels=self.waveform_channels, non_linearity=nlin,
+                       hidden_channels=self.waveform_channels),
+                LKA1d(self.waveform_channels, kernel_sizes=(129, 129), dilation=6, activation=self.nonlinearity),
+                TFNO1d(n_modes_height=self.n_fourier_modes, in_channels=self.waveform_channels,
+                       out_channels=self.flowthrough_channels, non_linearity=nlin, hidden_channels=self.waveform_channels),
+                nn.LayerNorm((self.target_latent_size,)),
             ))
             self.target_layers.append(nn.Sequential(
                 TFNO1d(n_modes_height=self.n_fourier_modes, in_channels=self.flowthrough_channels + 1,
                        out_channels=self.target_channels, non_linearity=nlin,
                        hidden_channels=self.target_channels),
+                LKA1d(self.target_channels, kernel_sizes=(129, 129), dilation=6, activation=self.nonlinearity),
                 TFNO1d(n_modes_height=self.n_fourier_modes, in_channels=self.target_channels, non_linearity=nlin,
                        out_channels=self.flowthrough_channels, hidden_channels=self.target_channels),
+                nn.LayerNorm((self.target_latent_size,)),
             ))
 
         self.wave_decoder = nn.Sequential(
             TFNO1d(n_modes_height=self.n_fourier_modes, in_channels=self.flowthrough_channels + 2,
                    out_channels=self.wave_decoder_channels, non_linearity=nlin,
                    hidden_channels=self.wave_decoder_channels),
+            nn.Linear(self.target_latent_size, self.target_latent_size),
+            nlin,
             TFNO1d(n_modes_height=self.n_fourier_modes, in_channels=self.wave_decoder_channels, non_linearity=nlin,
                    out_channels=self.n_ants * 2, hidden_channels=self.wave_decoder_channels),
             nn.Linear(self.target_latent_size, self.fft_len),
@@ -167,7 +170,7 @@ class GeneratorModel(FlatModule):
 
         '''self.example_input_array = (torch.zeros((1, 32, 2, self.fft_len)),
                                       torch.zeros((1, self.target_latent_size)),
-                                      torch.tensor([[1250]]), torch.tensor([[.4]]))'''
+                                      torch.Tensor([1250]), torch.Tensor([.4]))'''
 
     def forward(self, clutter, target, pulse_length, bandwidth) -> torch.tensor:
 
@@ -231,13 +234,13 @@ class GeneratorModel(FlatModule):
 
         # Get clutter spectrum into complex form and normalize to unit energy
         clutter_spectrum = torch.fft.fftshift(torch.complex(args[1][:, -1, 0, :], args[1][:, -1, 1, :]), dim=1)
-        clutter_spectrum = clutter_spectrum / torch.sqrt(torch.sum(clutter_spectrum * torch.conj(clutter_spectrum),
-                                                                   dim=1))[:, None]
+        # clutter_spectrum = clutter_spectrum / torch.sqrt(torch.sum(clutter_spectrum * torch.conj(clutter_spectrum),
+        #                                                            dim=1))[:, None]
 
         # Get target spectrum into complex form and normalize to unit energy
         target_spectrum = torch.fft.fftshift(torch.complex(args[2][:, 0, :], args[2][:, 1, :]), dim=1)
-        target_spectrum = target_spectrum / torch.sqrt(torch.sum(target_spectrum * torch.conj(target_spectrum),
-                                                                 dim=1))[:, None]
+        # target_spectrum = target_spectrum / torch.sqrt(torch.sum(target_spectrum * torch.conj(target_spectrum),
+        #                                                          dim=1))[:, None]
 
         # Get waveform into complex form and normalize it to unit energy
         gen_waveform = self.getWaveform(nn_output=args[0])
@@ -253,9 +256,7 @@ class GeneratorModel(FlatModule):
         kld = [.5 * (torch.trace(torch.linalg.pinv(eb) @ ea) + torch.trace(torch.linalg.pinv(ea) @ eb) + (mub - mua) @ (
             torch.linalg.pinv(ea) + torch.linalg.pinv(eb)) @ (mub - mua)) for ea, eb, mua, mub in zip(eas, ebs, muas, mubs)]
 
-        kld_loss = sum(kld)
-
-        # target_loss =
+        kld_loss = torch.sqrt(sum(kld) / target_stft.shape[0])
 
         # Target and clutter power functions
         targ_ac = torch.sum(
@@ -263,7 +264,7 @@ class GeneratorModel(FlatModule):
         clut_ac = torch.sum(
             torch.abs(torch.fft.ifft(clutter_spectrum.unsqueeze(1) * mfiltered, dim=2)), dim=1)
         target_loss = torch.nanmean(clut_ac / (EPS + targ_ac))
-        # target_loss = torch.nanmean(nn_func.cosine_similarity(targ_ac, clut_ac)**2)
+        # target_loss = torch.nanmean(nn_func.logsigmoid(clut_ac - targ_ac))# + kld_loss
 
         # Sidelobe loss functions
         slf = nn_func.threshold(torch.abs(torch.fft.ifft(mfiltered, dim=2)), 1e-9, 1e-9)
@@ -271,7 +272,7 @@ class GeneratorModel(FlatModule):
         theoretical_mainlobe_width = torch.max(torch.ceil(1 / (2 * args[5]))).int()
         pslrs = torch.max(sidelobe_func, dim=-1)[0] - torch.mean(sidelobe_func[..., theoretical_mainlobe_width:-theoretical_mainlobe_width], dim=-1)
         # pslrs = get_pslr(sidelobe_func.squeeze(1))
-        sidelobe_loss = 1. / (EPS + torch.nanmean(pslrs))
+        sidelobe_loss = (1. / (EPS + torch.nanmean(pslrs)))**2
         # sidelobe_loss = torch.tensor(0., device=self.device)
 
         # Bandwidth loss
@@ -283,16 +284,9 @@ class GeneratorModel(FlatModule):
             cross_sidelobe[cross_sidelobe == 0] = 1e-9
             cross_sidelobe = 10 * torch.log(cross_sidelobe / 10)
             ortho_loss = torch.nanmean(torch.abs(sidelobe_func.sum(dim=1)[:, 0]) /
-                                       (EPS + torch.abs(cross_sidelobe.sum(dim=1)[:, 0])))**2
-
-            # Apply hinge loss
-            ortho_loss = torch.abs(ortho_loss - .3)
+                                       (EPS + torch.abs(cross_sidelobe.sum(dim=1)[:, 0])))**2 - .09
         else:
             ortho_loss = torch.tensor(0., device=self.device)
-
-        # Make sure the losses are positive (they should always be)
-        sidelobe_loss = torch.abs(sidelobe_loss)
-        target_loss = torch.abs(target_loss)
 
         return {'target_loss': target_loss,
                 'sidelobe_loss': sidelobe_loss, 'ortho_loss': ortho_loss, 'kld_loss': kld_loss}
@@ -337,6 +331,8 @@ class GeneratorModel(FlatModule):
         opt.zero_grad()'''
         # self.clip_gradients(opt, gradient_clip_val=0.5, gradient_clip_algorithm="norm")
         if (batch_idx + 1) % self.config.accumulation_steps == 0:
+            if self.config.gradient_flow:
+                plot_grad_flow(self.named_parameters())
             opt = self.optimizers()
             opt.step()
             opt.zero_grad()
@@ -383,15 +379,38 @@ class GeneratorModel(FlatModule):
         results = self.forward(clutter_spec, target_enc, pulse_length, bandwidth)
         train_loss = self.loss_function(results, clutter_spec, target_spec, target_enc, pulse_length, bandwidth)
 
-        train_loss['loss'] = torch.sqrt(torch.abs(
-            train_loss['target_loss'] * (1 + train_loss['sidelobe_loss'] * self.global_step + train_loss['ortho_loss'])))
+        train_loss['loss'] = train_loss['target_loss'] * (1 + train_loss['sidelobe_loss'] * self.global_step + train_loss['ortho_loss'])
         return train_loss
 
-    def on_before_optimizer_step(self, optimizer: Optimizer) -> None:
+    '''def on_before_optimizer_step(self, optimizer: Optimizer) -> None:
         if self.global_step % self.config.log_epoch == 0:
             # norms = grad_norm(self, norm_type=2)  # Compute 2-norm for each layer
             norms = {**grad_norm(self.clutter_encoder, norm_type=2),
                      **grad_norm(self.clutter_squash, norm_type=2),
                      **grad_norm(self.clutter_target_init, norm_type=2),
                      **grad_norm(self.wave_decoder, norm_type=2)}
-            self.log_dict(norms)
+            self.log_dict(norms)'''
+
+if __name__ == '__main__':
+    from config import get_config
+    from torchviz import make_dot
+    from pytorch_lightning import seed_everything
+    torch.set_float32_matmul_precision('medium')
+    gpu_num = 1
+    device = f'cuda:{gpu_num}' if torch.cuda.is_available() else 'cpu'
+    seed_everything(np.random.randint(1, 2048), workers=True)
+    # seed_everything(43, workers=True)
+
+    target_config = get_config('wave_exp', './vae_config.yaml')
+    mdl = GeneratorModel(config=target_config)
+
+    dummy = (torch.zeros((1, 32, 2, target_config.fft_len)),
+                                      torch.zeros((1, target_config.target_latent_size)),
+                                      torch.tensor([1250]), torch.tensor([.4]))
+    output = mdl(*dummy)
+    import sys
+    sys.setrecursionlimit(20000)
+    dot = make_dot(output, params=dict(mdl.named_parameters()))
+
+    dot.format = 'png'
+    dot.render('wavemodel')